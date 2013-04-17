dace6502e1719a32ac3723747f2a9103f000415a
Wouter Lueks <wouter@telox.net>
2013-04-17 18:56:26 +0200

    Also add ProtocolInfo

---
+++ src/org/irmacard/web/restapi/util/ProtocolInfo.java
@@ -0,0 +1,9 @@
+package org.irmacard.web.restapi.util;
+
+import java.util.Map;
+
+public class ProtocolInfo {
+	public String qr_url;
+	public String status_url;
+	public Map<Short, String> verification_names;
+}

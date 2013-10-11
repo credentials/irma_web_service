bf383ee1973feab0e8bbba33d72b14542d01b699
Pim Vullers <pim@vullersmail.nl>
2013-10-11 15:50:27 +0200

    More IRMAwiki work

---
+++ src/org/irmacard/web/irmaWiki/IRMAWikiAttributeStoreServlet.java
@@ -0,0 +1,95 @@
+package org.irmacard.web.irmaWiki;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.jar.Attributes;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.irmacard.web.restapi.resources.irmaWiki.IRMAWikiVerificationResource;
+
+public class IRMAWikiAttributeStoreServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 5535714536228123508L;
+
+	/**
+     * Process HEAD request. This returns the same headers as GET request, but without content.
+	 * @throws IOException 
+     * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse).
+     */
+    protected void doHead(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        // Process request without content.
+        processRequest(request, response, false);
+    }
+
+    /**
+     * Process GET request.
+     * @throws IOException 
+     * @see HttpServlet#doGet(HttpServletRequest, HttpServletResponse).
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        // Process request with content.
+        processRequest(request, response, true);
+    }
+
+    /**
+     * Process the actual request.
+     * @param request The request to be processed.
+     * @param response The response to be created.
+     * @param content Whether the request body should be written (GET) or not (HEAD).
+     * @throws IOException If something fails at I/O level.
+     */
+    private void processRequest
+        (HttpServletRequest request, HttpServletResponse response, boolean content) throws IOException
+    {
+        // Get requested id by path info. Also, strip leading '/'
+        String id = request.getPathInfo().substring(1);
+
+    	if (id == null) {
+    		// If the "id" parameter is not present you are not allowed to access any attributes.
+    		response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    		return;
+    	}
+
+    	Attributes attributes = getAttributeStore().get(id);
+
+    	if (attributes == null) {
+    		// User hasn't authenticated with IRMA, so not allowed to access attributes.
+    		System.out.println("Attributes not registered: access prohibited");
+    		response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    		return;
+    	}
+
+        String php = "<?php $iwAttributeStore = array(";        
+        for (Iterator<Object> i = attributes.keySet().iterator(); i.hasNext();) {
+        	String name = (String) i.next();
+        	php += "'" + name + "' => '" + attributes.getValue(name) + "'" + (i.hasNext() ? ", " : ");"); 
+        }
+        response.getWriter().write(php);
+    }
+
+    private Map<String, Attributes> getAttributeStore() {
+		ServletContext servletctx = getServletContext();
+
+		@SuppressWarnings("unchecked")
+		Map<String, Attributes> store = (Map<String, Attributes>) servletctx
+				.getAttribute(IRMAWikiVerificationResource.ATTRIBUTE_STORE_NAME);
+		if (store == null) {
+			System.out.println("Resetting store in IRMAWikiAttributeStoreServlet");
+			store = new HashMap<String, Attributes>();
+			servletctx.setAttribute(IRMAWikiVerificationResource.ATTRIBUTE_STORE_NAME, store);
+		}
+
+		return store;
+	}
+}
--- src/org/irmacard/web/restapi/resources/irmaWiki/IRMAWikiVerificationResource.java
+++ src/org/irmacard/web/restapi/resources/irmaWiki/IRMAWikiVerificationResource.java
@@ -1,8 +1,11 @@
 package org.irmacard.web.restapi.resources.irmaWiki;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.ServletContext;
 
 import org.irmacard.credentials.Attributes;
 import org.irmacard.credentials.info.DescriptionStore;
@@ -13,8 +16,9 @@ import org.irmacard.web.restapi.util.ProtocolStep;
 
 public class IRMAWikiVerificationResource extends
 		VerificationBaseResource {
-	final static String VERIFIER = "IRMAWiki";
-	final static String VERIFICATIONID = "memberAll";
+	final static String VERIFIER = "IRMATube";
+	final static String VERIFICATIONID = "memberType";
+	public static final String ATTRIBUTE_STORE_NAME = "IRMAWiki.attribute.store";
 
 	VerificationDescription memberDescription;
 
@@ -38,19 +42,14 @@ public class IRMAWikiVerificationResource extends
 			return ProtocolStep.newError(memberDescription.getName()
 					+ " credential is invalid/expired");
 		}
-
-		if (!(new String(member.get("type"))).equals("user")) {
-			return ProtocolStep.newError("You need to be a member to modify this content");
-		}
 		
-		// FIXME: store nickName + type + OK in session.
-
-		String servletURL = getRootRef().getPath();
-		String serviceURL = servletURL.substring(0,servletURL.lastIndexOf('/'));
-
+		getAttributeStore().put(id, member);
+		
+		System.out.println("refPath: " + getReference().getPath() + "\n refQuery: " + getReference().getQuery());
+		System.err.println("refPath: " + getReference().getPath() + "\n refQuery: " + getReference().getQuery());
 		ps.protocolDone = true;
 		ps.status = "success";
-		ps.result = getBaseURL() + serviceURL + "/resources/" + id;
+		ps.result = "http://localhost/mediawiki/index.php?Special:IRMALogin/" + id;
 		return ps;
 	}
 
@@ -60,5 +59,38 @@ public class IRMAWikiVerificationResource extends
 		List<VerificationDescription> result = new ArrayList<VerificationDescription>();
 		result.add(memberDescription);
 		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<String, Attributes> getAttributeStore() {
+		Map<String, Attributes> store = null;
+		ServletContext ctxt = null;
+		ServletContext other_ctxt = null;
+
+		try {
+			ctxt = (ServletContext) getContext().getServerDispatcher().getContext().getAttributes().get("org.restlet.ext.servlet.ServletContext");
+			other_ctxt = (ServletContext) getContext().getAttributes().get("org.restlet.ext.servlet.ServletContext");
+		} catch (ClassCastException e) {
+			e.printStackTrace();
+		}
+
+		// Try first context
+		if(ctxt != null) {
+			store = (Map<String, Attributes>) ctxt.getAttribute(ATTRIBUTE_STORE_NAME);
+		}
+
+		// Try second context
+		if(store == null && other_ctxt != null) {
+			store = (Map<String, Attributes>) other_ctxt.getAttribute(ATTRIBUTE_STORE_NAME);
+		}
+
+		// Still no store found, create it and put it in first context.
+		if( store == null) {
+			System.out.println("Store not found, generating new store");
+			store = new HashMap<String, Attributes>();
+			ctxt.setAttribute(ATTRIBUTE_STORE_NAME, store);
+		}
+
+		return store;
 	}
 }

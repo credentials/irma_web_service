97a5baf91bca93386e471899591182bea351fd55
Wouter Lueks <wouter@telox.net>
2015-07-19 15:46:56 +0200

    Convert to gretty

--- data/vouchers/vouchers.sql
+++ data/vouchers/vouchers.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS vouchers;
 CREATE TABLE vouchers
 (
 id int unsigned not null auto_increment primary key,
 voucher_code varchar(40),
 user_id varchar(40)
 );
+INSERT INTO vouchers VALUES (1, 'AABBCC', 's123456');
+INSERT INTO vouchers VALUES (2, 'ZYXZYX', 's654321');

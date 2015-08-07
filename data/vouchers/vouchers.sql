7d892d49b855ccdaa20fd09de90cbf225ac5db11
Wouter Lueks <wouter@telox.net>
2015-08-07 16:30:37 -0400

    Cleanup gradle and H2 memory database

--- data/vouchers/vouchers.sql
+++ data/vouchers/vouchers.sql
@@ -5,5 +5,5 @@ id int unsigned not null auto_increment primary key,
 voucher_code varchar(40),
 user_id varchar(40)
 );
-INSERT INTO vouchers VALUES (1, 'AABBCC', 's123456');
-INSERT INTO vouchers VALUES (2, 'ZYXZYX', 's654321');
+INSERT INTO vouchers VALUES (1, 'AABBCC', NULL);
+INSERT INTO vouchers VALUES (2, 'ZYXZYX', NULL);
